b6e1ff16b5e75990c68ebad7702e38021d351181
anishazahanKuiperz <anisha.zahan@kuiperz.io>
2024-06-09 01:15:55 +0600

    first page responsive

--- index.js
+++ index.js
@@ -1,198 +1,204 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const menuButton = document.getElementById("menuButton");
-  const menuContent = document.getElementById("menuContent");
+  const menuBtn = document.getElementById("menu-btn");
+  const menuContent = document.getElementById("menu-content");
+  const closeIcon = document.getElementById("close-icon");
 
-  if (menuButton && menuContent) {
-    menuButton.addEventListener("click", function () {
-      menuContent.classList.toggle("hidden");
-    });
-  }
+  // Show menu content initially
+  // menuContent.style.display = "block";
 
-  // .........five page ..........
-  const fifthPageToggleData = [
-    {
-      id: 1,
-      title: "EMERGENCY CONTACTS",
-      data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
-    },
-    {
-      id: 2,
-      title: "HEALTH RISKS",
-      data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
-    },
-    {
-      id: 3,
-      title: "VACCINATIONS",
-      data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
-    },
-    {
-      id: 4,
-      title: "MEDICATIONS",
-      data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
-    },
-    {
-      id: 5,
-      title: "MENTAL HEALTH",
-      data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
-    },
-    {
-      id: 6,
-      title: "HEALTHCARE FACILITIES",
-      data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
-    },
-    {
-      id: 7,
-      title: "PHARMACIES",
-      data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
-    },
-  ];
+  // Add event listener to menu button to show the menu content
+  menuBtn.addEventListener("click", function () {
+    menuContent.style.display = "block";
+  });
 
-  const container = document.getElementById("toggle-container");
-
-  if (container) {
-    fifthPageToggleData.forEach((item) => {
-      const itemDiv = document.createElement("div");
-      const itemDivContent = document.createElement("div");
-      itemDivContent.className =
-        "flex border-b pb-1 justify-between items-center cursor-pointer";
-
-      const titleP = document.createElement("p");
-      titleP.className = "font-semibold text-sm text-slate uppercase";
-      titleP.innerText = item.title;
-
-      const toggleP = document.createElement("p");
-      toggleP.className = "text-xl font-medium text-slate";
-      toggleP.innerText = "+";
-
-      const dataP = document.createElement("p");
-      dataP.className = "text-sm p-3 hidden";
-      dataP.innerText = item.data;
-
-      itemDiv.addEventListener("click", () => {
-        if (dataP.classList.contains("hidden")) {
-          dataP.classList.remove("hidden");
-          toggleP.innerText = "-";
-        } else {
-          dataP.classList.add("hidden");
-          toggleP.innerText = "+";
-        }
-      });
-      itemDiv.appendChild(itemDivContent);
-      itemDivContent.appendChild(titleP);
-      itemDivContent.appendChild(toggleP);
-      itemDiv.appendChild(dataP);
-
-      container.appendChild(itemDiv);
-    });
-  }
-
-  // .........six page ..........
-  const sixthPageToggleData = [
-    {
-      id: 1,
-      title: "VISA REQUIREMENTS",
-      data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
-    },
-    {
-      id: 2,
-      title: "TRAVEL INSURANCE",
-      data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
-    },
-    {
-      id: 3,
-      title: "PERSONAL SAFETY",
-      data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
-    },
-    {
-      id: 4,
-      title: "CLIMATE",
-      data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
-    },
-    {
-      id: 5,
-      title: "COMMUNICATIONS",
-      data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
-    },
-    {
-      id: 6,
-      title: "DRIVING & VEHICLE SAFETY",
-      data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
-    },
-    {
-      id: 7,
-      title: "LOCAL LAWS & CULTURES",
-      data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
-    },
-    {
-      id: 8,
-      title: "CRIME",
-      data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
-    },
-    {
-      id: 9,
-      title: "TERRORISM",
-      data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
-    },
-    {
-      id: 10,
-      title: "AIR RAID SIRENS",
-      data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
-    },
-    {
-      id: 11,
-      title: "UNEXPLODED ORDNANCE (UXO)",
-      data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
-    },
-    {
-      id: 12,
-      title: "EMERGENCY CONTACTS",
-      data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
-    },
-  ];
-
-  const mainContainer = document.getElementById(
-    "toggle-container-for-six-page"
-  );
-
-  if (mainContainer) {
-    sixthPageToggleData.forEach((item) => {
-      const itemDiv = document.createElement("div");
-      const itemDivContent = document.createElement("div");
-      itemDivContent.className =
-        "flex border-b pb-1 justify-between items-center cursor-pointer";
-
-      const titleP = document.createElement("p");
-      titleP.className = "font-semibold text-sm text-slate uppercase";
-      titleP.innerText = item.title;
-
-      const toggleP = document.createElement("p");
-      toggleP.className = "text-xl font-medium text-slate";
-      toggleP.innerText = "+";
-
-      const dataP = document.createElement("p");
-      dataP.className = "text-sm p-3 hidden";
-      dataP.innerText = item.data;
-
-      itemDiv.addEventListener("click", () => {
-        if (dataP.classList.contains("hidden")) {
-          dataP.classList.remove("hidden");
-          toggleP.innerText = "-";
-        } else {
-          dataP.classList.add("hidden");
-          toggleP.innerText = "+";
-        }
-      });
-
-      itemDiv.appendChild(itemDivContent);
-      itemDivContent.appendChild(titleP);
-      itemDivContent.appendChild(toggleP);
-
-      itemDiv.appendChild(dataP);
-
-      mainContainer.appendChild(itemDiv);
-    });
-  }
+  // Add event listener to close icon to hide the menu content
+  closeIcon.addEventListener("click", function () {
+    menuContent.style.display = "none";
+  });
 });
+
+// .........five page ..........
+const fifthPageToggleData = [
+  {
+    id: 1,
+    title: "EMERGENCY CONTACTS",
+    data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
+  },
+  {
+    id: 2,
+    title: "HEALTH RISKS",
+    data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
+  },
+  {
+    id: 3,
+    title: "VACCINATIONS",
+    data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
+  },
+  {
+    id: 4,
+    title: "MEDICATIONS",
+    data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
+  },
+  {
+    id: 5,
+    title: "MENTAL HEALTH",
+    data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
+  },
+  {
+    id: 6,
+    title: "HEALTHCARE FACILITIES",
+    data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
+  },
+  {
+    id: 7,
+    title: "PHARMACIES",
+    data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
+  },
+];
+
+const container = document.getElementById("toggle-container");
+
+if (container) {
+  fifthPageToggleData.forEach((item) => {
+    const itemDiv = document.createElement("div");
+    const itemDivContent = document.createElement("div");
+    itemDivContent.className =
+      "flex border-b pb-1 justify-between items-center cursor-pointer";
+
+    const titleP = document.createElement("p");
+    titleP.className = "font-semibold text-sm text-slate uppercase";
+    titleP.innerText = item.title;
+
+    const toggleP = document.createElement("p");
+    toggleP.className = "text-xl font-medium text-slate";
+    toggleP.innerText = "+";
+
+    const dataP = document.createElement("p");
+    dataP.className = "text-sm p-3 display-hidden";
+    dataP.innerText = item.data;
+
+    itemDiv.addEventListener("click", () => {
+      if (dataP.classList.contains("display-hidden")) {
+        dataP.classList.remove("display-hidden");
+        toggleP.innerText = "-";
+      } else {
+        dataP.classList.add("display-hidden");
+        toggleP.innerText = "+";
+      }
+    });
+    itemDiv.appendChild(itemDivContent);
+    itemDivContent.appendChild(titleP);
+    itemDivContent.appendChild(toggleP);
+    itemDiv.appendChild(dataP);
+
+    container.appendChild(itemDiv);
+  });
+}
+
+// .........six page ..........
+const sixthPageToggleData = [
+  {
+    id: 1,
+    title: "VISA REQUIREMENTS",
+    data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
+  },
+  {
+    id: 2,
+    title: "TRAVEL INSURANCE",
+    data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
+  },
+  {
+    id: 3,
+    title: "PERSONAL SAFETY",
+    data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
+  },
+  {
+    id: 4,
+    title: "CLIMATE",
+    data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
+  },
+  {
+    id: 5,
+    title: "COMMUNICATIONS",
+    data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
+  },
+  {
+    id: 6,
+    title: "DRIVING & VEHICLE SAFETY",
+    data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
+  },
+  {
+    id: 7,
+    title: "LOCAL LAWS & CULTURES",
+    data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
+  },
+  {
+    id: 8,
+    title: "CRIME",
+    data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
+  },
+  {
+    id: 9,
+    title: "TERRORISM",
+    data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
+  },
+  {
+    id: 10,
+    title: "AIR RAID SIRENS",
+    data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
+  },
+  {
+    id: 11,
+    title: "UNEXPLODED ORDNANCE (UXO)",
+    data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
+  },
+  {
+    id: 12,
+    title: "EMERGENCY CONTACTS",
+    data: "Before you travel to Ukraine check that you have appropriate travel insurance that is valid in Ukraine for local treatment and/or unexpected medical evacuation. If you're not insured.",
+  },
+];
+
+const mainContainer2 = document.getElementById("toggle-container-for-six-page");
+
+if (mainContainer2) {
+  sixthPageToggleData.forEach((item) => {
+    const itemDiv = document.createElement("div");
+    const itemDivContent = document.createElement("div");
+    itemDivContent.className =
+      "flex border-b pb-1 justify-between items-center cursor-pointer";
+
+    const titleP = document.createElement("p");
+    titleP.className = "font-semibold text-sm text-slate uppercase";
+    titleP.innerText = item.title;
+
+    const toggleP = document.createElement("p");
+    toggleP.className = "text-xl font-medium text-slate";
+    toggleP.innerText = "+";
+
+    const dataP = document.createElement("p");
+    dataP.className = "text-sm p-3 display-hidden";
+    dataP.innerText = item.data;
+
+    itemDiv.addEventListener("click", () => {
+      if (dataP.classList.contains("display-hidden")) {
+        dataP.classList.remove("display-hidden");
+        toggleP.innerText = "-";
+      } else {
+        dataP.classList.add("display-hidden");
+        toggleP.innerText = "+";
+      }
+    });
+
+    itemDiv.appendChild(itemDivContent);
+    itemDivContent.appendChild(titleP);
+    itemDivContent.appendChild(toggleP);
+
+    itemDiv.appendChild(dataP);
+
+    mainContainer2.appendChild(itemDiv);
+  });
+}
 
 // .........seven  page ..........
 const sevenPageToggleData = [
@@ -258,11 +264,11 @@ const sevenPageToggleData = [
   },
 ];
 
-const mainContainer = document.getElementById(
+const mainContainer3 = document.getElementById(
   "toggle-container-for-seven-page"
 );
 
-if (mainContainer) {
+if (mainContainer3) {
   sevenPageToggleData.forEach((item) => {
     const itemDiv = document.createElement("div");
     const leftDiv = document.createElement("div");
@@ -284,15 +290,15 @@ if (mainContainer) {
     img.className = "w-10 h-6";
 
     const dataP = document.createElement("p");
-    dataP.className = "text-sm p-3 hidden";
+    dataP.className = "text-sm p-3 display-hidden";
     dataP.innerText = item.data;
 
     itemDiv.addEventListener("click", () => {
-      if (dataP.classList.contains("hidden")) {
-        dataP.classList.remove("hidden");
+      if (dataP.classList.contains("display-hidden")) {
+        dataP.classList.remove("display-hidden");
         toggleP.innerText = "-";
       } else {
-        dataP.classList.add("hidden");
+        dataP.classList.add("display-hidden");
         toggleP.innerText = "+";
       }
     });
@@ -306,6 +312,6 @@ if (mainContainer) {
 
     itemDiv.appendChild(dataP);
 
-    mainContainer.appendChild(itemDiv);
+    mainContainer3.appendChild(itemDiv);
   });
 }
